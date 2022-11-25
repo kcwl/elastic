126f48476bdfa98dd948fc0e8dc66e6f1ca799e8
kcwl <767899122@qq.com>
2022-11-25 15:59:27 +0800

    reflect interface

---
+++ include/elastic/detail/reflect.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string_view>
+#include <elastic/tuple_size.hpp>
+
+namespace elastic
+{
+	namespace detail
+	{
+		template<typename _Ty>
+		constexpr auto struct_name() -> std::string_view
+		{
+			constexpr auto f = []<template _Ty>()
+			{
+				using namespace std::string_view_literals;
+
+				constexpr std::string_view name = __FUNCSIG__""sv;
+
+				return name.substr(114, name.size() - 121);
+			};
+
+			return f<_Ty>();
+		}
+
+		template<std::size_t N, typename T>
+		constexpr auto get(const T& val)
+		{
+			return std::get<N>(detail::template make_tuple(val, size_t_<detail::tuple_size_v<T>>{}));
+		}
+	}
+}
