fbd0823c01480b20c77bf0614c837ee1f5c083ce
kcwl <767899122@qq.com>
2022-11-28 11:35:26 +0800

    code format

--- include/elastic/detail/concepts.hpp
+++ include/elastic/detail/concepts.hpp
@@ -3,27 +3,24 @@
 
 namespace elastic
 {
-	namespace detail
-	{
-		template<typename _Ty, std::size_t N>
-		concept aggregate_inialize = is_aggregate_initialize<_Ty, N>::value;
+    namespace detail
+    {
+        template <typename _Ty, std::size_t N>
+        concept aggregate_inialize = is_aggregate_initialize<_Ty, N>::value;
 
-		template<typename _Ty>
-		concept copable = std::is_copy_constructible_v<std::remove_all_extents_t<_Ty>> &&
-								std::is_move_constructible_v<std::remove_all_extents_t<_Ty>> &&
-								std::is_move_constructible_v<std::remove_all_extents_t<_Ty>>;
+        template <typename _Ty>
+        concept copable = std::is_copy_constructible_v<std::remove_all_extents_t<_Ty>> &&
+                          std::is_move_constructible_v<std::remove_all_extents_t<_Ty>> &&
+                          std::is_move_constructible_v<std::remove_all_extents_t<_Ty>>;
 
+        template <typename _Ty>
+        concept reflection = copable<_Ty> && !
+        std::is_polymorphic_v<_Ty> && (std::is_aggregate_v<_Ty> || std::is_scalar_v<_Ty>);
 
-		template<typename _Ty>
-		concept reflection = copable<_Ty> && !std::is_polymorphic_v<_Ty> && (std::is_aggregate_v<_Ty> || std::is_scalar_v<_Ty>);
+        template <typename _Ty>
+        concept tuple = requires() { std::tuple_size<_Ty>(); };
 
-		template<typename _Ty>
-		concept tuple = requires()
-		{
-			std::tuple_size<_Ty>();
-		};
-
-		template<typename _Ty>
-		concept class_cpt = std::is_class_v<_Ty>;
-	}
-}
+        template <typename _Ty>
+        concept class_cpt = std::is_class_v<_Ty>;
+    } // namespace detail
+} // namespace elastic
