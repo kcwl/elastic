76bc0818d4f579d5f3977ca930d2c7eaf0adb7ea
kcwl <767899122@qq.com>
2022-12-13 15:16:50 +0800

    basic type complete

--- include/elastic/type.hpp
+++ include/elastic/type.hpp
@@ -1,26 +1,18 @@
 #pragma once
-#include <elastic/detail/require.hpp>
 #include <optional>
 #include <string>
 
 namespace elastic
 {
-	using int32 = int32_t;
+	using int8 = int8_t;
 
-	using uint32 = uint32_t;
+	using int16 = int16_t;
+
+	using int32 = int32_t;
 
 	using int64 = int64_t;
 
-	using uint64 = uint64_t;
-
 	using string = std::string;
 
-	template <typename _Ty>
-	using optional = std::optional<_Ty>;
-
-	template <typename _Ty>
-	using require = detail::require<_Ty>;
-
-	template<typename _Ty>
-	using vector = std::vector<_Ty>;
+	using bytes = std::vector<std::byte>;
 } // namespace elastic
