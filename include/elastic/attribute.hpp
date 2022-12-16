85ead8ab32cda4ce5ab87840f60b95d623b01eee
kcwl <767899122@qq.com>
2022-12-16 12:01:00 +0800

    support for attribute

--- include/elastic/attribute.hpp
+++ include/elastic/attribute.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <elastic/detail/concepts.hpp>
 #include <optional>
 #include <vector>
 
@@ -10,148 +11,96 @@ namespace elastic
 		struct fixed
 		{
 			using type = _Ty;
+
+			template <typename _Integer>
+			fixed& operator=(const _Integer& other)
+			{
+				value_ = other;
+
+				return *this;
+			}
+
+			type value_;
+		};
+
+		template <bool flag, typename _Ty>
+		struct basic_optional : std::optional<_Ty>
+		{
+			constexpr static bool require_value = false;
 		};
 
 		template <typename _Ty>
-		class require_construct_base
+		struct basic_optional<true, _Ty> : std::optional<_Ty>
 		{
-			using value_type = std::remove_cvref_t<_Ty>;
-
-		public:
-			template <typename... _Args>
-			constexpr _Ty& construct(_Args&&... args) noexcept(std::is_nothrow_constructible_v<_Ty, _Args...>)
-			{
-				std::_Construct_in_place(this->value, std::forward<_Args>(args)...);
-
-				has_value_ = true;
-
-				return this->value_;
-			}
-
-			constexpr _Ty& operator*() & noexcept
-			{
-				return this->value_;
-			}
-
-			constexpr _Ty& operator*() const& noexcept
-			{
-				return this->value_;
-			}
-
-			constexpr _Ty&& operator*() && noexcept
-			{
-				return std::move(this->value_);
-			}
-
-			constexpr _Ty&& operator*() const&& noexcept
-			{
-				return std::move(this->value_);
-			}
-
-		public:
-			bool has_value_;
-
-			value_type value_;
-		};
-
-		template <typename _Ty>
-		class basic_require : private require_construct_base<_Ty>
-		{
-		public:
-			template <typename... _Args>
-			requires(std::is_constructible_v<_Ty, _Args...>)
-			constexpr explicit basic_require(_Args&&... args) noexcept(std::is_nothrow_constructible_v<_Ty, _Args...>)
-				: require_construct_base<_Ty>(std::forward<_Args>(args)...)
-			{}
-
-			template <typename _Elem, typename... _Args>
-			requires(std::is_constructible_v<_Ty, std::initializer_list<_Elem>&, _Args...>)
-			constexpr explicit basic_require(std::initializer_list<_Elem> init, _Args&&... args)
-				: require_construct_base<_Ty>(init, std::forward<_Args>(args)...)
-			{}
-
-		public:
-			constexpr _Ty* operator->() const noexcept
-			{
-				return std::addressof(this->value_);
-			}
-
-			constexpr _Ty* operator->() noexcept
-			{
-				return std::addressof(this->value_);
-			}
-
-		public:
-			template <typename... _Args>
-			constexpr void emplace(_Args&&... args) noexcept
-			{
-				this->construct(std::forward<_Args>(args)...);
-			}
-
-			[[nodiscard]] constexpr const _Ty& value() const& noexcept
-			{
-				return this->value_;
-			}
-
-			[[nodiscard]] constexpr _Ty& value() & noexcept
-			{
-				return this->value_;
-			}
-
-			[[nodiscard]] constexpr _Ty&& value() && noexcept
-			{
-				return std::move(this->value_);
-			}
-
-			[[nodiscard]] constexpr const _Ty&& value() const&& noexcept
-			{
-				return std::move(this->value_);
-			}
-
-			bool has_value()
-			{
-				return this->has_value_;
-			}
+			constexpr static bool require_value = true;
 		};
 	} // namespace attr
 
-	using fixed32 = attr::fixed<uint32_t>::type;
-	using fixed64 = attr::fixed<uint64_t>::type;
+	using fixed32 = attr::fixed<uint32_t>;
+	using fixed64 = attr::fixed<uint64_t>;
 
 	template <typename _Ty>
-	using optional = std::optional<_Ty>;
+	struct is_fixed : std::false_type
+	{};
+
+	template <typename _Ty>
+	struct is_fixed<attr::fixed<_Ty>> : std::true_type
+	{};
+
+	template <typename _Ty>
+	using optional = attr::basic_optional<false, _Ty>;
 
 	template <typename _Ty>
 	using repeated = std::vector<_Ty>;
 
 	template <typename _Ty>
-	using require = attr::basic_require<_Ty>;
+	using require = attr::basic_optional<true, _Ty>;
+
+	template <typename _Ty1, typename _Ty2, typename _Result>
+	struct second_type : std::bool_constant<std::is_same_v<_Ty1, _Ty2>>
+	{
+		using type = _Result;
+	};
+
+	template <detail::signed_integer _Ty>
+	struct unsign
+	{
+		using value_type =
+			typename std::disjunction<second_type<_Ty, int8_t, uint8_t>, second_type<_Ty, int16_t, uint16_t>,
+									  second_type<_Ty, int32_t, uint32_t>, second_type<_Ty, int64_t, uint64_t>>::type;
+
+		template <typename _Integer>
+		unsign& operator=(const _Integer& other)
+		{
+			value_ = other;
+
+			return *this;
+		}
+
+		value_type value_;
+	};
 
 	template <typename _Ty>
-	struct unsign;
+	struct is_unsign : std::false_type
+	{};
 
-	template <>
-	struct unsign<int8_t>
-	{
-		using type = uint8_t;
-	};
+	template <typename _Ty>
+	struct is_unsign<unsign<_Ty>> : std::true_type
+	{};
 
-	template <>
-	struct unsign<int16_t>
-	{
-		using type = uint16_t;
-	};
+	template <typename _Ty>
+	concept optional_t = requires(_Ty value) {
+							 value.has_value();
+							 *value;
+						 };
 
-	template <>
-	struct unsign<int32_t>
-	{
-		using type = uint32_t;
-	};
+	template <typename _Ty>
+	concept fixed_t = is_fixed<_Ty>::value;
 
-	template <>
-	struct unsign<int64_t>
-	{
-		using type = uint64_t;
-	};
+	template <typename _Ty>
+	concept unsign_t = is_unsign<_Ty>::value;
+
+	template <typename _Ty>
+	concept attribute = optional_t<_Ty> || fixed_t<_Ty> || unsign_t<_Ty>;
 
 } // namespace elastic
