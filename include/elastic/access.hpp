387dde96806da220c413d9df15a34896c946bec2
kcwl <767899122@qq.com>
2022-12-30 16:20:47 +0800

    perfect serialization of inherit structure

--- include/elastic/access.hpp
+++ include/elastic/access.hpp
@@ -3,12 +3,34 @@
 
 namespace elastic
 {
-	struct access
+	class access
 	{
+	public:
 		template<typename _Archive, typename _Ty>
 		static void serialize(_Archive& ar, _Ty& t)
 		{
 			t.serialize(ar);
 		}
+
+		template<typename _Ty, typename _U>
+		static _Ty& cast_reference(_U& u)
+		{
+			return static_cast<_Ty&>(u);
+		}
+
+		template <typename _Ty, typename _U>
+		static _Ty* cast_pointer(_U* u)
+		{
+			return static_cast<_Ty*>(u);
+		}
 	};
+
+	namespace serialize
+	{
+		template<typename _Base, typename _Derived>
+		std::conditional_t<std::is_const_v<_Derived>, const _Base, _Base>& base_object(_Derived& d)
+		{
+			return access::cast_reference<_Base, _Derived>(d);
+		}
+	}
 }
