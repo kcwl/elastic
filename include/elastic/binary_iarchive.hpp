0913fec5ebea89b7cb436fb564edfe07b7394895
kcwl <767899122@qq.com>
2023-01-02 23:00:53 +0800

    add dev

--- include/elastic/binary_iarchive.hpp
+++ include/elastic/binary_iarchive.hpp
@@ -1,58 +1,72 @@
 #pragma once
 #include <elastic/binary_primitive.hpp>
 #include <elastic/iserialize.hpp>
+#include <elastic/serialize_streambuf.hpp>
 #include <istream>
 
 namespace elastic
 {
-	template<typename _Archive, typename _Elem, typename _Traits>
+	template <typename _Archive, typename _Elem, typename _Traits>
 	class binary_iarchive_impl : public binary_iprimitive<_Archive, _Elem, _Traits>
 	{
 	public:
-		explicit binary_iarchive_impl(std::basic_streambuf<_Elem>& sb)
+		binary_iarchive_impl(std::basic_streambuf<_Elem, _Traits>& sb)
 			: binary_iprimitive<_Archive, _Elem, _Traits>(sb)
-		{
-
-		}
+		{}
 
 		binary_iarchive_impl(std::istream& is)
 			: binary_iprimitive<_Archive, _Elem, _Traits>(*is.rdbuf())
-		{
+		{}
 
-		}
+		explicit binary_iarchive_impl(serialize_streambuf<_Elem, _Traits>& sb)
+			: buf_(sb)
+			, binary_iprimitive<_Archive, _Elem, _Traits>(sb)
+		{}
 
 	public:
-		template<typename _Ty>
+		template <typename _Ty>
 		_Archive& operator>>(_Ty& t)
 		{
-			serialize::load(*this->archive(), t);
+			try
+			{
+				buf_.start_transaction();
+
+				serialize::load(*this->archive(), t);
+			}
+			catch (...)
+			{
+				buf_.roll_back();
+			}
 
 			return *this->archive();
 		}
 
-		template<typename _Ty>
+		template <typename _Ty>
 		_Archive& operator&(_Ty& t)
 		{
 			*this->archive() >> t;
 
 			return *this->archive();
 		}
+
+	private:
+		serialize_streambuf<_Elem, _Traits> buf_;
 	};
 
-	class binary_iarchive 
+	class binary_iarchive
 		: public binary_iarchive_impl<binary_iarchive, std::istream::char_type, std::istream::traits_type>
 	{
 	public:
 		explicit binary_iarchive(std::istream& is)
 			: binary_iarchive_impl<binary_iarchive, std::istream::char_type, std::istream::traits_type>(is)
-		{
-
-		}
+		{}
 
 		explicit binary_iarchive(std::streambuf& bsb)
 			: binary_iarchive_impl<binary_iarchive, std::istream::char_type, std::istream::traits_type>(bsb)
-		{
+		{}
 
-		}
+		explicit binary_iarchive(serialize_streambuf<std::istream::char_type, std::istream::traits_type>& sb)
+			: binary_iarchive_impl<binary_iarchive, std::istream::char_type, std::istream::traits_type>(sb)
+		{}
 	};
-}
+} // namespace elastic
