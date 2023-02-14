a08e3ed3ea946aaeb40a826f503f07e7c593423e
kcwl <767899122@qq.com>
2023-02-14 13:33:04 +0800

    fix for save enum

--- include/elastic/archive/oserialize.hpp
+++ include/elastic/archive/oserialize.hpp
@@ -118,13 +118,15 @@ namespace elastic
 		template <typename _Archive, typename _Ty>
 		inline void save(_Archive& ar, _Ty&& t)
 		{
+			using type = std::remove_reference_t<_Ty>;
+
 			using typex = std::conditional_t<
-				std::is_pointer_v<_Ty>, detail::identify_t<save_pointer_type<_Archive>>,
+				std::is_pointer_v<type>, detail::identify_t<save_pointer_type<_Archive>>,
 				std::conditional_t<
-					std::is_enum_v<_Ty>, detail::identify_t<save_enum_type<_Archive>>,
+					std::is_enum_v<type>, detail::identify_t<save_enum_type<_Archive>>,
 					std::conditional_t<
-						std::is_array_v<_Ty>, detail::identify_t<save_array_type<_Archive>>,
-						std::conditional_t<attribute_t<_Ty>, detail::identify_t<save_optional_type<_Archive>>,
+						std::is_array_v<type>, detail::identify_t<save_array_type<_Archive>>,
+						std::conditional_t<attribute_t<type>, detail::identify_t<save_optional_type<_Archive>>,
 										   detail::identify_t<save_non_pointer_type<_Archive>>>>>>;
 
 			typex::invoke(ar, std::forward<_Ty>(t));
