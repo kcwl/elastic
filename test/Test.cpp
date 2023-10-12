68262c2b76cbc264e0ecb123929447f2c1e773b2
kcwl <767899122@qq.com>
2023-10-12 15:42:18 +0800

    unit test for generate

--- test/Test.cpp
+++ test/Test.cpp
@@ -2,7 +2,7 @@
 #include <boost/test/unit_test.hpp>
 #include "attr.h"
 //#include "exception.h"
-//#include "generate.h"
+#include "generate.h"
 //#include "inherit.h"
 #include "type.h"
 //#include "tuple_size.h"
