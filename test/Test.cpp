7cc5d8aff9c8f0b2a357018af3472ea16ff5a11f
kcwl <767899122@qq.com>
2023-10-12 15:43:59 +0800

    unit test for exception

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE Test
 #include <boost/test/unit_test.hpp>
 #include "attr.h"
-//#include "exception.h"
+#include "exception.h"
 #include "generate.h"
 #include "inherit.h"
 #include "type.h"
