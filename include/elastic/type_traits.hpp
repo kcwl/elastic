e235aca9f029f8a139f849ed87b13d7ab3f705cf
kcwl <767899122@qq.com>
2023-10-27 16:36:06 +0800

    fix warning for linux that because of requires

--- include/elastic/type_traits.hpp
+++ include/elastic/type_traits.hpp
@@ -138,13 +138,11 @@ namespace elastic
 
 	template <typename _Ty>
 	concept copable = std::is_copy_constructible_v<std::remove_all_extents_t<_Ty>> &&
-					  std::is_move_constructible_v<std::remove_all_extents_t<_Ty>> &&
 					  std::is_move_constructible_v<std::remove_all_extents_t<_Ty>>;
 
 	template <typename _Ty>
-	concept reflection = requires {
+	concept reflection =
 		copable<_Ty> && !std::is_polymorphic_v<_Ty> && (std::is_aggregate_v<_Ty> || std::is_scalar_v<_Ty>);
-	};
 
 	template <typename _Ty>
 	concept tuple_t = requires() { std::tuple_size<_Ty>(); };
