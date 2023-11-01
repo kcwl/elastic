be9c20e3a07c76507f9d06439fc127daa29243bb
kcwl <767899122@qq.com>
2023-11-01 10:22:54 +0800

    clone protobuf with git

--- shell/install_3rd.sh
+++ shell/install_3rd.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 apt-get install libboost-all-dev -y
+
+apt-get install git -y
+
+git clone https://github.com/protocolbuffers/protobuf.git
+
+git switch -C v21.12
+
+./configure
+
+make -j8
+
+make install
