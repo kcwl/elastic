62339db324e7602d3b23390dc6e05bde4b41b9a2
kcwl <767899122@qq.com>
2023-11-03 14:06:05 +0800

    fix for message lite name

--- include/elastic.hpp
+++ include/elastic.hpp
@@ -2,5 +2,5 @@
 #include "elastic/access.hpp"
 #include "elastic/archive.hpp"
 #include "elastic/flex_buffer.hpp"
-#include "elastic/message_module.hpp"
+#include "elastic/message_lite.hpp"
 #include "elastic/reflect.hpp"
